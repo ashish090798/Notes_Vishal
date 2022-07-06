d5545d6cec399e0e29b3a8d1ad3f9f59cdd74cee
Ashish Tiwari <78926157+ashish090798@users.noreply.github.com>
2022-07-06 19:46:20 +0530

    Add files via upload

---
+++ R/Recommendation system/recommenderlab.R
@@ -0,0 +1,24 @@
+#Load the package and prepare a dataset
+install.packages("recommenderlab")
+library("recommenderlab")
+data("MovieLense")
+#The format of MovieLense is an object of class "realRatingMatrix"
+
+## look at the first few ratings of the first user
+head(as(MovieLense[1,], "list")[[1]])
+
+
+### use only users with more than 100 ratings
+MovieLense100 <- MovieLense[rowCounts(MovieLense) >100,]
+MovieLense100
+
+#Train a UBCF recommender using a small training set
+train <- MovieLense100[1:50]
+rec <- Recommender(train, method = "UBCF")
+rec
+
+#Create top-N recommendations for new users (users 101 and 102)
+pre <- predict(rec, MovieLense100[101:102], n = 10)
+pre
+
+as(pre, "list")
